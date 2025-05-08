c84e44fb69ea8d3574ba105283dd530924aed715
TechMarryn <136928439+TechMarryn@users.noreply.github.com>
2025-05-08 14:07:07 +0200

    ghostty config, dunst config

--- stow.sh
+++ stow.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-stow --adopt -t ~ btop dolphin ghostty git hypr kde keepassxc rofi ssh waybar zen zsh
+stow --adopt -t ~ btop dolphin dunst ghostty git hypr kde keepassxc rofi ssh waybar zen zsh
